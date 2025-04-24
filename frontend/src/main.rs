f4577d6ff01d897e62972922c779f564799a2232
Ohad Lutzky <ohad@lutzky.net>
2025-04-24 22:52:58 +0100

    Send pings

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,4 @@
+use gloo_console::info;
 use gloo_net::http::Request;
 use yew::prelude::*;
 
@@ -16,25 +17,41 @@ struct PatientListProps {
 }
 
 #[function_component(PatientList)]
-fn patient_list(PatientListProps{patients}:&PatientListProps) -> Html {
-    patients.iter().map(|patient| html! {
-        <div class="patient" style="border: 1px solid black; padding: 10px;">
-            <h1>{ &patient.name }</h1>
-            <p>{ "Patient details go here." }</p>
-            <button>{ "Ping" }</button>
-        </div>
-    }).collect()
+fn patient_list(PatientListProps { patients }: &PatientListProps) -> Html {
+    patients
+        .iter()
+        .map(|patient| {
+            let ping_them = {
+                let patient = patient.clone();
+                Callback::from(move |_: MouseEvent| {
+                    wasm_bindgen_futures::spawn_local(async move {
+                        info!("Sending ping for patient", patient.id);
+                        Request::post(&format!("/api/patients/{}/ping", patient.id))
+                            .send()
+                            .await
+                            .unwrap();
+                    })
+                })
+            };
+            html! {
+            <div class="patient" style="border: 1px solid black; padding: 10px;">
+                <h1>{ &patient.name }</h1>
+                <p>{ "Patient details go here." }</p>
+                <button onclick={ping_them}>{ "Ping" }</button>
+            </div>}
+        })
+        .collect()
 }
 
 #[function_component]
 fn App() -> Html {
-    let patients = use_state(||vec![]);
+    let patients = use_state(|| vec![]);
     {
         let patients = patients.clone();
         use_effect_with((), move |_| {
             let patients = patients.clone();
             wasm_bindgen_futures::spawn_local(async move {
-                let fetched_patients:Vec<model::Patient> = Request::get("/api/patients")
+                let fetched_patients: Vec<model::Patient> = Request::get("/api/patients")
                     .send()
                     .await
                     .unwrap()
