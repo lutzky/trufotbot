707648e3de59a171689e44a6c1199b8790553927
Ohad Lutzky <ohad@lutzky.net>
2025-05-19 22:37:19 +0100

    patient_detail refactor

--- frontend/src/components/patient_detail.rs
+++ frontend/src/components/patient_detail.rs
@@ -14,9 +14,13 @@ use crate::{
 };
 
 use anyhow::{Result, bail};
-use shared::api::{medication::MedicationSummary, requests::PatientCreateRequest, responses};
+use shared::api::{
+    medication::MedicationSummary,
+    requests::PatientCreateRequest,
+    responses::{self, PatientGetResponse},
+};
 
-async fn delete(patient_id: i64) -> Result<()> {
+async fn api_delete(patient_id: i64) -> Result<()> {
     let api_url = format!("/api/patients/{}", patient_id);
     let res = Request::delete(&api_url).send().await?;
     if !res.ok() {
@@ -29,7 +33,7 @@ async fn delete(patient_id: i64) -> Result<()> {
     Ok(())
 }
 
-async fn fetch(patient_id: i64) -> Result<responses::PatientGetResponse> {
+async fn api_fetch(patient_id: i64) -> Result<responses::PatientGetResponse> {
     let api_url = format!("/api/patients/{}", patient_id);
     let res = Request::get(&api_url).send().await?;
     if !res.ok() {
@@ -42,7 +46,7 @@ async fn fetch(patient_id: i64) -> Result<responses::PatientGetResponse> {
     Ok(res.json().await?)
 }
 
-async fn update_settings(patient_id: i64, req: &PatientCreateRequest) -> Result<()> {
+async fn api_update_settings(patient_id: i64, req: &PatientCreateRequest) -> Result<()> {
     let api_url = format!("/api/patients/{}", patient_id);
     let res = Request::put(&api_url).json(req)?.send().await?;
     if !res.ok() {
@@ -88,110 +92,79 @@ fn patient_medication_summary_card(props: &PatientMedicationSummaryCardProps) ->
     }
 }
 
+type ResponseState = UseStateHandle<Option<Result<PatientGetResponse>>>;
+
+fn make_fetch_callback(response: ResponseState, patient_id: i64) -> Callback<()> {
+    Callback::from(move |_: ()| {
+        let response = response.clone();
+
+        wasm_bindgen_futures::spawn_local(async move {
+            response.set(None);
+            let res = api_fetch(patient_id).await;
+            log_if_error("Failed to fetch patient details", &res);
+            response.set(Some(res));
+        });
+    })
+}
+
+fn make_save_settings_callback(
+    fetch_callback: Callback<()>,
+    patient_id: i64,
+) -> Callback<PatientCreateRequest> {
+    Callback::from(move |req: PatientCreateRequest| {
+        let fetch_callback = fetch_callback.clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            let res = api_update_settings(patient_id, &req).await;
+            log_if_error("Failed to update patient settings", &res);
+            if res.is_ok() {
+                fetch_callback.emit(());
+            }
+        });
+    })
+}
+
+fn make_delete_callback(navigator: Navigator, patient_id: i64) -> Callback<MouseEvent> {
+    Callback::from(move |_: MouseEvent| {
+        let navigator = navigator.clone();
+        wasm_bindgen_futures::spawn_local(async move {
+            if !gloo_dialogs::confirm("Are you sure you want to delete this patient?") {
+                return;
+            }
+            let res = api_delete(patient_id).await;
+            log_if_error("Failed to update patient settings", &res);
+            if res.is_ok() {
+                navigator.push(&Route::Home);
+            }
+        });
+    })
+}
+
 #[derive(Properties, PartialEq)]
 pub struct PatientDetailProps {
     pub id: i64, // Received from the router
 }
 
 #[function_component(PatientDetail)]
-pub fn patient_detail(props: &PatientDetailProps) -> Html {
-    let patient_id = props.id;
-    let patient_get_response = use_state(|| None::<Result<responses::PatientGetResponse>>);
+pub fn patient_detail(PatientDetailProps { id: patient_id }: &PatientDetailProps) -> Html {
+    let response = use_state(|| None::<Result<responses::PatientGetResponse>>);
 
-    let fetch_callback = {
-        let patient_get_response = patient_get_response.clone();
-        Callback::from(move |_: ()| {
-            let patient_get_response = patient_get_response.clone();
-
-            wasm_bindgen_futures::spawn_local(async move {
-                patient_get_response.set(None);
-                let res = fetch(patient_id).await;
-                log_if_error("Failed to fetch patient details", &res);
-                patient_get_response.set(Some(res));
-            });
-        })
-    };
+    let delete_callback = make_delete_callback(use_navigator().unwrap(), *patient_id);
+    let fetch_callback = make_fetch_callback(response.clone(), *patient_id);
+    let save_settings_callback = make_save_settings_callback(fetch_callback.clone(), *patient_id);
 
     {
         let fetch_callback = fetch_callback.clone();
         use_effect_with((), move |_| fetch_callback.emit(()));
     }
 
-    let save_settings_callback = {
-        let fetch_callback = fetch_callback.clone();
-        Callback::from(move |req: PatientCreateRequest| {
-            let fetch_callback = fetch_callback.clone();
-            wasm_bindgen_futures::spawn_local(async move {
-                let res = update_settings(patient_id, &req).await;
-                log_if_error("Failed to update patient settings", &res);
-                if res.is_ok() {
-                    fetch_callback.emit(());
-                }
-            });
-        })
-    };
-
-    let delete_callback = {
-        let navigator = use_navigator().unwrap();
-        Callback::from(move |_| {
-            let navigator = navigator.clone();
-            wasm_bindgen_futures::spawn_local(async move {
-                if !gloo_dialogs::confirm("Are you sure you want to delete this patient?") {
-                    return;
-                }
-                let res = delete(patient_id).await;
-                log_if_error("Failed to update patient settings", &res);
-                if res.is_ok() {
-                    navigator.push(&Route::Home);
-                }
-            });
-        })
-    };
-
-    let content =
-        error_handling::error_waiting_or(patient_get_response.as_ref(), move |response| {
-            let (taken, never_taken): (Vec<_>, Vec<_>) = response
-                .medications
-                .iter()
-                .partition(|med| med.last_taken_at.is_some());
-            html! {
-                <>
-                    <h1>{ format!("Medications for {}", &response.name) }</h1>
-                    // These should show last-taken, humanized, and be sorted by that
-                    { taken.iter().map(|&medication| {
-                    html! {
-                        <PatientMedicationSummaryCard
-                            patient_id={patient_id}
-                            medication_summary={medication.clone()}/>
-                    }
-                }).collect::<Html>() }
-                    // TODO: This hr is drawn even if there are no never-taken medications
-                    <hr />
-                    { never_taken.iter().map(|&medication| {
-                    html! {
-                        <PatientMedicationSummaryCard
-                            patient_id={patient_id}
-                            medication_summary={medication.clone()}/>
-                    }
-                }).collect::<Html>() }
-                    <hr />
-                    <details>
-                        <summary>{ "Edit patient" }</summary>
-                        <PatientSettings
-                            group=false
-                            name={response.name.clone()}
-                            telegram_group_id={response.telegram_group_id}
-                            onsave={save_settings_callback.clone()}
-                        />
-                        <div class="grid">
-                            <button onclick={delete_callback.clone()} class="contrast">
-                                { "Delete Patient" }
-                            </button>
-                        </div>
-                    </details>
-                </>
-            }
-        });
+    let content = error_handling::error_waiting_or(response.as_ref(), move |response| {
+        render_content(
+            response,
+            *patient_id,
+            delete_callback.clone(),
+            save_settings_callback.clone(),
+        )
+    });
 
     let new_medication = html! {
         <details>
@@ -208,6 +181,55 @@ pub fn patient_detail(props: &PatientDetailProps) -> Html {
             { content }
             <hr />
             { new_medication }
+        </>
+    }
+}
+
+fn render_content(
+    response: &PatientGetResponse,
+    patient_id: i64,
+    delete_callback: Callback<MouseEvent>,
+    save_settings_callback: Callback<PatientCreateRequest>,
+) -> Html {
+    let (taken, never_taken): (Vec<_>, Vec<_>) = response
+        .medications
+        .iter()
+        .partition(|med| med.last_taken_at.is_some());
+    html! {
+        <>
+            <h1>{ format!("Medications for {}", &response.name) }</h1>
+            // These should show last-taken, humanized, and be sorted by that
+            { taken.iter().map(|&medication| {
+            html! {
+                <PatientMedicationSummaryCard
+                    patient_id={patient_id}
+                    medication_summary={medication.clone()}/>
+            }
+        }).collect::<Html>() }
+            // TODO: This hr is drawn even if there are no never-taken medications
+            <hr />
+            { never_taken.iter().map(|&medication| {
+            html! {
+                <PatientMedicationSummaryCard
+                    patient_id={patient_id}
+                    medication_summary={medication.clone()}/>
+            }
+        }).collect::<Html>() }
+            <hr />
+            <details>
+                <summary>{ "Edit patient" }</summary>
+                <PatientSettings
+                    group=false
+                    name={response.name.clone()}
+                    telegram_group_id={response.telegram_group_id}
+                    onsave={save_settings_callback.clone()}
+                />
+                <div class="grid">
+                    <button onclick={delete_callback.clone()} class="contrast">
+                        { "Delete Patient" }
+                    </button>
+                </div>
+            </details>
         </>
     }
 }
