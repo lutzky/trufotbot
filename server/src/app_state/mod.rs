2547db40a8906466923a46f1f35f3acfed3a63fb
Ohad Lutzky <ohad@lutzky.net>
2025-05-24 22:18:54 +0100

    Unify reminder sending

--- server/src/app_state/mod.rs
+++ server/src/app_state/mod.rs
@@ -1,11 +1,10 @@
-use anyhow::bail;
-use axum::extract::FromRef;
+use axum::extract::{FromRef, Path, State};
 
 use sqlx::SqlitePool;
 
 use crate::{
-    messenger::{Messenger, SentMessageInfo},
-    models::{Medication, Patient},
+    handlers::reminders::send_reminder,
+    messenger::Messenger,
     reminder_scheduler::{MedicationId, PatientId, ReminderScheduler},
     storage::Storage,
 };
@@ -35,100 +34,32 @@ impl FromRef<AppState> for Storage {
     }
 }
 
-async fn send_reminder_callback_hacky_version(
-    db: SqlitePool,
-    messenger: Messenger,
-    patient_id: PatientId,
-    medication_id: MedicationId,
-) -> anyhow::Result<()> {
-    // TODO: Non-hacky version
-    let patient = match Patient::get(&db, patient_id.0).await {
-        Ok(Some(p)) => p,
-        Ok(None) => {
-            bail!("Patient not found {patient_id:?}");
-        }
-        Err(e) => {
-            bail!("Database error: {}", e);
-        }
-    };
-    let medication = match Medication::get(&db, medication_id.0).await {
-        Ok(Some(p)) => p,
-        Ok(None) => {
-            bail!("Medication not found {medication_id:?}");
-        }
-        Err(e) => {
-            bail!("Database error: {}", e);
-        }
-    };
-
-    let base_message =
-        teloxide::utils::markdown::escape(&format!("Time to take {}.", medication.name));
-
-    let message_id = messenger
-        .send_message(&patient, base_message.clone())
-        .await
-        .unwrap()
-        .unwrap();
-
-    messenger
-        .edit_message(
-            &patient,
-            message_id.id(),
-            format!(
-                "{base_message} {}",
-                teloxide::utils::markdown::link("http://example.com", "Take")
-            ),
-        )
-        .await
-        .unwrap();
-
-    Ok(())
-
-    // Here you would call the actual reminder sending logic
-    // crate::handlers::reminders::send_reminder(...)
-}
-
 impl AppState {
     pub async fn new(db: SqlitePool, telegram_bot: Option<teloxide::Bot>) -> anyhow::Result<Self> {
         let messenger = Messenger::new(telegram_bot);
+        let storage = Storage { pool: db };
         let callback = {
             let messenger = messenger.clone();
-            let db = db.clone();
+            let storage = storage.clone();
             move |patient_id: PatientId, medication_id: MedicationId| {
                 let messenger = messenger.clone();
-                let db = db.clone();
+                let storage = storage.clone();
                 tokio::spawn(async move {
-                    if let Err(e) = send_reminder_callback_hacky_version(
-                        db,
-                        messenger,
-                        patient_id,
-                        medication_id,
+                    if let Err(e) = send_reminder(
+                        State(storage),
+                        State(messenger),
+                        Path((patient_id.0, medication_id.0)),
                     )
                     .await
                     {
-                        log::error!("Failed to send reminder: {}", e);
+                        log::error!("Failed to send reminder: {e:?}");
                     }
                 });
-                // let patient = db.get
-                // OK, what we need to do is move get_patient and get_medication
-                // into a Model module, so we can refer to it at this point. MAYBE! Actually, maybe Patient::get is good enough. But first, let's try doing this the ugly hacky way:
-                // let patient = Patient::get(&db, patient_id.0)
-                //     .await
-                //     .map_err(|e| {
-                //         log::error!("Database error: {}", e);
-                //         (
-                //             StatusCode::INTERNAL_SERVER_ERROR,
-                //             "Failed to fetch patient".to_string(),
-                //         )
-                //     })?
-                //     .ok_or((StatusCode::NOT_FOUND, "Patient not found".to_string()))
-                //         log::warn!("Reminder callback for {patient_id:?}, {medication_id:?}");
-                //     }
             }
         };
 
         Ok(AppState {
-            storage: Storage { pool: db },
+            storage,
             messenger,
             reminder_scheduler: ReminderScheduler::new(callback).await?,
         })
