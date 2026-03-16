b8d4d2b5b466bb8bc2a13ab925bad2953b808f56
Ohad Lutzky <ohad@lutzky.net>
2026-03-16 22:47:47 +0000

    frontend: Format e2e tests as well

--- frontend/e2e/vue.spec.ts
+++ frontend/e2e/vue.spec.ts
@@ -1,20 +1,23 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from '@playwright/test'
 
 // See here how to get started:
 // https://playwright.dev/docs/intro
 test('visits the app root url', async ({ page }) => {
-  const mockPatients = [{ "id": 1, "name": "Alice" }, { "id": 3, "name": "Bob" }]
-  await page.route('**/api/patients', async route => {
+  const mockPatients = [
+    { id: 1, name: 'Alice' },
+    { id: 3, name: 'Bob' },
+  ]
+  await page.route('**/api/patients', async (route) => {
     await route.fulfill({
       status: 200,
       contentType: 'application/json',
-      body: JSON.stringify(mockPatients)
+      body: JSON.stringify(mockPatients),
     })
   })
 
-  await page.goto('/');
-  await expect(page.locator('h1')).toHaveText('Select Patient');
+  await page.goto('/')
+  await expect(page.locator('h1')).toHaveText('Select Patient')
 
-  await expect(page.getByRole('button', { name: 'Alice' })).toBeVisible();
-  await expect(page.getByRole('button', { name: 'Bob' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Alice' })).toBeVisible()
+  await expect(page.getByRole('button', { name: 'Bob' })).toBeVisible()
 })
