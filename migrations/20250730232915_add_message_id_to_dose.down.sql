9e0b3e7ad7fe35e0885066ae3247eb5b678eab98
Ohad Lutzky <ohad@lutzky.net>
2026-03-28 21:09:41 +0000

    Fix migrations
    These were broken by adding REUSE headers

--- migrations/20250730232915_add_message_id_to_dose.down.sql
+++ migrations/20250730232915_add_message_id_to_dose.down.sql
@@ -1,6 +1,2 @@
--- Copyright (C) 2026 Ohad Lutzky <lutzky@gmail.com>
---
--- SPDX-License-Identifier: GPL-3.0-only
-
 ALTER TABLE doses DROP telegram_group_id;
 ALTER TABLE doses DROP telegram_message_id;
